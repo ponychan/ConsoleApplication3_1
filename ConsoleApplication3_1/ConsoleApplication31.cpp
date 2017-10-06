df2f872ee4ca04596b7b34d950610b8877534d59
ponychan <rzsckbmx@163.com>
2017-10-06 11:38:37 +0800

    3.1.cpp
    利用函数计算x的n次方

--- ConsoleApplication3_1/ConsoleApplication31.cpp
+++ ConsoleApplication3_1/ConsoleApplication31.cpp
@@ -1,11 +1,20 @@
 // ConsoleApplication31.cpp: 定义控制台应用程序的入口点。
-//
+//3.1.cpp计算x的n次方的函数
 
 #include "stdafx.h"
+#include<iostream>
+using namespace std;
 
+double power(double x, int n) {
+	double val = 1.0;
+	while (n--)
+		val *= x;
+	return val;
+}
 
 int main()
 {
-    return 0;
+	cout << " 5 to the power 2 is " << power(5, 2) << endl;
+	return 0;
 }
 
